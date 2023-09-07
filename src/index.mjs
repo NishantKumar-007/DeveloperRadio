039f676c2861aad9b6870f39767ad121abdadca4
NishantKumar-007 <61771311+NishantKumar-007@users.noreply.github.com>
2023-09-07 18:44:09 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+var val = document.getElementById("search");
+
+// val.addEventListener('mouseenter',activity)
+
+// function activity(){
+//   console.log("submitted");
+// }
